4d6a69ef8ad2cede427c5f6dff7ee3cbab465526
Zwart <jzwart@usgs.gov>
2024-01-24 14:53:13 -0800

    getting rid of baseline models we dont need

--- baseline_models/run_aquatics_baselines.R
+++ baseline_models/run_aquatics_baselines.R
@@ -6,6 +6,7 @@ print(paste0("Running Creating Aquatics baselines at ", Sys.time()))
 print(paste0("Running daily climatology at ", Sys.time()))
 source("baseline_models/models/aquatics_climatology.R")
 print(paste0("Completed daily climatology ", Sys.time()))
+
 print(paste0("Running daily persistence at ", Sys.time()))
 source("baseline_models/models/aquatics_persistenceRW.R")
 print(paste0("Completed daily persistence ", Sys.time()))
