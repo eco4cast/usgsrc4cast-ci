bd0d94f8a45d2d76f0bd6849f2331fe6472f7188
Zwart <jzwart@usgs.gov>
2024-06-12 15:11:25 -0700

    making sure we dont add example

--- catalog/forecasts/forecast_models.R
+++ catalog/forecasts/forecast_models.R
@@ -239,7 +239,7 @@ for (i in 1:length(config$variable_groups)){ ## organize variable groups
       forecast_sites <- c()
 
       ## LOOP OVER MODEL IDS AND CREATE JSONS
-      for (m in theme_models$model_id){
+      for (m in var_models$model_id){
 
         # make model items directory
         if (!dir.exists(paste0(catalog_config$forecast_path,'/',names(config$variable_groups)[i],'/',var_formal_name,"/models"))){
