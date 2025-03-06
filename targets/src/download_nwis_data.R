c25dee74203c6ef76649d1ec60850b8f290e7a0f
Jacob Zwart <jzwart@usgs.gov>
2025-03-06 09:18:58 -0800

    updates to rfu function and rm old files

--- targets/src/download_nwis_data.R
+++ targets/src/download_nwis_data.R
@@ -1,6 +1,21 @@
 
 
-
+#' Download Historic Chlorophyll Data
+#'
+#' This function retrieves historic chlorophyll data from the NWIS database for specified sites
+#' and date ranges, filters by a minimum chlorophyll concentration, and outputs the data as an RDS file.
+#'
+#' @param sites A character vector of site numbers for which to download data.
+#' @param start_date The start date for the data retrieval in 'YYYY-MM-DD' format.
+#' @param end_date The end date for the data retrieval in 'YYYY-MM-DD' format.
+#' @param pcodes A character vector of parameter codes for the data to be retrieved.
+#' @param service A character string specifying the data service to use (e.g., "dv").
+#' @param statCd An optional character string for the statistic code to filter the data.
+#' @param min_chl A numeric value specifying the minimum chlorophyll concentration (in µg/L) to retain in the output. Default is 0.
+#' @param out_file A character string indicating the path to the output RDS file where the data will be saved.
+#'
+#' @return A character string indicating the path to the saved RDS file.
+#' @export
 download_historic_data <- function(
     sites,
     start_date,
@@ -8,7 +23,7 @@ download_historic_data <- function(
     pcodes,
     service,
     statCd = NULL,
-    min_chl = 0, # minimum chlorohpyll-a to keep
+    min_chl = 0, # minimum chlorohpyll to keep
     out_file
 ){
 
@@ -31,14 +46,28 @@ download_historic_data <- function(
   return(out_file)
 }
 
-
+#' Download Historic Chlorophyll UV Data
+#'
+#' This function retrieves historic chlorophyll UV data from the NWIS database for specified sites
+#' and date ranges, filters by a minimum chlorophyll concentration, and outputs the data as an RDS file.
+#'
+#' @param sites A character vector of site numbers for which to download data.
+#' @param start_date The start date for the data retrieval in 'YYYY-MM-DD' format.
+#' @param end_date The end date for the data retrieval in 'YYYY-MM-DD' format.
+#' @param pcodes A character vector of parameter codes for the data to be retrieved.
+#' @param service A character string specifying the data service to use (e.g., "uv").
+#' @param min_chl A numeric value specifying the minimum chlorophyll concentration (in µg/L) to retain in the output. Default is 0.
+#' @param out_file A character string indicating the path to the output RDS file where the data will be saved.
+#'
+#' @return A character string indicating the path to the saved RDS file.
+#' @export
 download_historic_uv_data <- function(
     sites,
     start_date,
     end_date,
     pcodes,
     service,
-    min_chl = 0, # minimum chlorohpyll-a to keep
+    min_chl = 0, # minimum chlorohpyll to keep
     out_file
 ){
 
@@ -60,13 +89,29 @@ download_historic_uv_data <- function(
   return(out_file)
 }
 
+#' Download Historic Chlorophyll RFU Data
+#'
+#' This function retrieves historic chlorophyll RFU data from the NWIS database for specified sites
+#' and date ranges, applies a conversion to chlorophyll concentration, filters by a minimum value,
+#' and outputs the data as an RDS file.
+#'
+#' @param sites A character vector of site numbers for which to download data.
+#' @param start_date The start date for the data retrieval in 'YYYY-MM-DD' format.
+#' @param end_date The end date for the data retrieval in 'YYYY-MM-DD' format.
+#' @param pcodes A character vector of parameter codes for the data to be retrieved.
+#' @param service A character string specifying the data service to use (e.g., "uv").
+#' @param min_chl A numeric value specifying the minimum chlorophyll concentration (in µg/L) to retain in the output. Default is 0.
+#' @param out_file A character string indicating the path to the output RDS file where the data will be saved.
+#'
+#' @return A character string indicating the path to the saved RDS file.
+#' @export
 download_historic_uv_rfu_data <- function(
     sites,
     start_date,
     end_date,
     pcodes,
     service,
-    min_chl = 0, # minimum chlorohpyll-a to keep
+    min_chl = 0, # minimum chlorohpyll to keep
     out_file
 ){
 
@@ -82,9 +127,10 @@ download_historic_uv_rfu_data <- function(
     filter(!is.na(chl_RFU), chl_RFU >= min_chl) %>%
     mutate(dateTime = as.Date(dateTime),
            # multiplying by factor of 4 will be sufficient in most cases
-           #  14211010 is offset high by 0.5 ug/L
-           chl_ug_L = case_when(site_no == "14211010" ~ chl_RFU * 4 - .5,
-                                TRUE ~ chl_RFU * 4)) %>%
+           #  14211010 is offset high by 0.2 ug/L
+           chl_ug_L = case_when(site_no == "14211010" ~ chl_RFU * 4 - .2,
+                                TRUE ~ chl_RFU * 4),
+           chl_ug_L = ifelse(chl_ug_L < 0, 0, chl_ug_L)) %>%
     group_by(site_no, dateTime) %>%
     summarise(chl_ug_L = mean(chl_ug_L), .groups = "drop")
 
